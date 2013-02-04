318f327b2bf0748b2c53a5331a4303ad71164e2c
Kevin Ho <keho98@gmail.com>
2013-02-03 18:57:55 -0800

    Adding quick test

--- app.js
+++ app.js
@@ -4,16 +4,20 @@ var app = require('express')()
 
 server.listen(8080);
 
+app.set('points', {});
+
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
-io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
+io.sockets.on('connection', function (socket) { 
   socket.on('my other event', function (data) {
     console.log(data);
   });
   socket.on('report point', function(data){
-  	console.log(data);
+    var arr = app.get('points');	
+    arr[data.key] = data.point;
+    app.set('points', arr);
+    console.log(data);
   });
 });
